532340e02605243391636daee608d094fe697560
gsp <cgsp740178088@163.com>
2018-04-07 16:19:59 +0800

    '项目初始化及webpack初始配置'

---
+++ build/webpack.config.js
@@ -0,0 +1,24 @@
+// 利用path这个包，来完成绝对路径的书写，因为用相对路径的话，可能会出现系统之间的差异等问题
+const path = require('path');
+module.exports = {
+  // 应用的入口
+  entry: {
+    app: path.join(__dirname, '../client/app.js')
+  },
+  // 打包的输出
+  // []是变量的意思，hash的话，就是根据文件内容，去计算得出的hash值
+  // 如app.xjjxjsjjd11224sss.js
+  // 这样就可以达到最大化地使用浏览器的缓存
+  output: {
+    // 打包输出文件的文件名
+    filename: '[name].[hash].js',
+    // 打包输出文件的存放地址
+    path: path.join(__dirname, '../dist'),
+    // 静态资源文件的引用路径
+    // 如果是''，引用路径是:app.hash.js
+    // 如果是'/public'，引用路径是/public/app.hash.js
+    // 这个东西非常有用，可以帮我们去区分某个url是静态资源，还是api的请求，或者是某些需要特殊处理的请求
+    publicPath: '/public'
+
+  }
+}

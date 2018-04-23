897981bd4a854423f012e7c01f5734becf21153f
gsp <cgsp740178088@126.com>
2018-04-23 20:56:05 +0800

    '页面布局初始化'

---
+++ client/views/layout/app-bar.jsx
@@ -0,0 +1,34 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { withStyles } from 'material-ui/styles';
+// import AppBar from 'material-ui/AppBar';
+// import ToolBar from 'material-ui/Toolbar';
+// import Button from 'material-ui/Button';
+// import Typography from 'material-ui/Typography';
+// import IconButton from 'material-ui/IconButton';
+// import HomeIcon from 'material-ui-icons/Home';
+
+// class MainAppBar extends React.Component {
+//   constructor() {
+//     super();
+//   }
+
+//   onHomeIconClick(event) {
+//     // console.log(event.target);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <AppBar position="fixed">
+//           <IconButton color="contrast" onClick={e => this.onHomeIconClick(e)}>
+//             <HomeIcon />
+//           </IconButton>
+//           <Typography type="title" color="inherit">
+//             gspNode
+//           </Typography>
+//         </AppBar>
+//       </div>
+//     );
+//   }
+// }

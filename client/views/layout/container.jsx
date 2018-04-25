f9a3411ab34731a5ca769eafc024d003bbbfd58b
gsp <cgsp740178088@126.com>
2018-04-25 18:23:53 +0800

    'container'

---
+++ client/views/layout/container.jsx
@@ -0,0 +1,29 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import Paper from 'material-ui/Paper';
+// import { withStyles } from 'material-ui/styles';
+
+// const styles = {
+//   root: {
+//     margin: 24,
+//     marginTop: 80,
+//   },
+// }
+
+// // 这个组件不需要用class写
+// const Container = ({ classes, children }) => {
+//   <Paper elevation={4} className={classes.root}>
+//     {children}
+//   </Paper>
+// }
+
+// // PropTypes.element---任何在组件里面可以渲染的东西，都可以定义为element
+// Container.propTypes = {
+//   classes: PropTypes.object.isRequired,
+//   children: PropTypes.oneOfType([
+//     PropTypes.arrayOf(PropTypes.element),
+//     PropTypes.element,
+//   ]),
+// }
+
+// export default withStyles(styles)(Container);
